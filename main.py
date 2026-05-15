31535aa1e40eb33e4f86843f1c3e551a19e16829
ARIS06PAP <mc24067@mail.ntua.gr>
2026-05-15 18:02:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import base64
 st.set_page_config(page_title="Coffee Lab Rewards", page_icon="☕", layout="centered")
 
 # 🚨 PRODUCTION GOOGLE APPS SCRIPT URL LOCKED
-SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzZuKYqrFcHMs7W_0W00tSvQjDRe2G5GjYfyj8_MtqJpqxGie2P9Vv43iuOR2t_f8ZLvw/exec"
+SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwsKZHjAg8QSyM3TkKFjU1nmM7rfWxeQxvDmkg1wXYSZanmWkGj_P8MgjsImoOKMm4zBA/exec"
 
 # --- THE ULTIMATE GLOBAL MOBILE RESET & CENTER (CSS INJECT) ---
 st.markdown("""
